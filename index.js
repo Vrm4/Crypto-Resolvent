9663ee0bc40758606710149e79c842c9d27c957e
Rıdvan Demirhan <71084215+Vrm4@users.noreply.github.com>
2023-02-23 19:53:49 +0300

    Continue

--- index.js
+++ index.js
@@ -6,12 +6,41 @@ const axios = require('axios')
 app.use(express.json())
 app.use(express.static('public'))
 
+const slugPromise = async(slug) => { 
+    const ApiUrl = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=68fc8f78-8978-4067-8d00-026993ad8422&slug=${slug.toLowerCase()}`
+    return await axios.get(ApiUrl)
+}
+const symbolPromise = async(symbol) =>{
+    const ApiUrl = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=68fc8f78-8978-4067-8d00-026993ad8422&symbol=${symbol}`
+    return await axios.get(ApiUrl)
+}
+
 app.post('/get-data' , (req,res) => { 
-    console.log(req.body)
-    const ApiUrl = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=68fc8f78-8978-4067-8d00-026993ad8422&symbol=BTC'
-    axios.get(ApiUrl) 
-      .then(res => console.log(res.data.data.BTC[0].quote))
-      .catch(err => console.log(err))
+    const searchData = req.body.searchData
+    const checkData = async () => { 
+        const symbol = symbolPromise(searchData)
+        const result = await symbol;
+        let keys = Object.keys(result.data.data);
+        let errorCode ;
+        try {
+            const slug = slugPromise(searchData)
+            const resultS = await slug
+            let keysS = Object.keys(resultS.data.data);
+            res.send({DataSlug : resultS.data.data[keysS[0]]})
+
+        } catch (error) {
+            errorCode = error.response.status
+        }
+        if( result.data.data[keys[0]].length === 0 && errorCode === 400){
+            res.sendStatus(400)
+        }
+        else{
+            if(result.data.data[keys[0]].length != 0 && errorCode != undefined){
+                res.send({DataSymbol : result.data.data[keys[0]]})
+            }
+        }
+    }
+    checkData()
 }) 
 
 app.listen(port , () => { 
--- public/index.js
+++ public/index.js
@@ -3,5 +3,11 @@ const content = document.querySelector('.contentWrapper')
 document.addEventListener('submit' , ( e )  =>{
     e.preventDefault()
     axios.post('/get-data' , { searchData : e.target[0].value})
-    .then((res) => { console.log(res)})
+    .then((res) => { 
+        content.style.display = 'block'
+        console.log(res)
+
+    })
+    .catch(() => {alert('Invalid Coin , Please Check Your Information')})
 }) 
+
